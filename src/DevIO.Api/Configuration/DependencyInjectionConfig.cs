3a2ae4ebc413a9b5b5057daabb7ef3e3b5890922
Davi Carvalho <davi_carvalho1@live.com>
2021-06-10 18:00:43 -0300

    feat: add swagger

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -7,6 +7,8 @@ using DevIO.Api.Extensions;
 using DevIO.Business.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace DevIO.Api.Configuration
 {
@@ -25,6 +27,8 @@ namespace DevIO.Api.Configuration
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
             return services;
         }
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -3,6 +3,7 @@ using Dev.Data.Context;
 using DevIO.Api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,10 +33,12 @@ namespace DevIO.Api
 
             services.WebApiConfig();
 
+            services.AddSwaggerConfig();
+
             services.ResolveDependecies();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -50,6 +53,8 @@ namespace DevIO.Api
 
             app.UseAuthentication();
             app.UseMvcConfiguration();
+
+            app.UseSwaggerConfig(provider);
         }
     }
 }
